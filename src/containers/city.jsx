95624692f58abe4a8a608d10815bd62886cee007
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-06-02 14:20:54 +0200

    starting redux

---
+++ src/containers/city.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+class City extends  Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+
+    return(
+      <div>
+
+
+
+      </div>
+
+
+
+      );
+  }
+}
+
+
+export default City;
---
+++ src/containers/city_list.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class CityList extends Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  render(){
+
+    return(
+
+      );
+  }
+
+}
+
+export default CityList;
